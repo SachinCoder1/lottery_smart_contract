e77e26371264dc924c6cbbfdfd4fdb466a33ed4f
sachincoder1 <sachinhlo232@gmail.com>
2022-05-28 20:22:47 +0530

    Adding initial lottery contract with public variables.

--- smart_lottery.sol
+++ smart_lottery.sol
@@ -1,1 +1,15 @@
-// Starting the lottery project with full energy.
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.24;
+
+
+
+contract Lottery {
+    address[] public allPlayers; // dynamic array with all player's address;
+    address public manager;  // Owner;
+    
+    constructor(){
+        // adding the address of owner
+        manager = msg.sender;
+    }
+    
+}
